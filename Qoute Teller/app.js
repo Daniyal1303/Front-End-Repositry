f8ef7a79d6aa6ffdc1ded243bdf72ecafe504aa7
Daniyal <daniyalahmad1303@gmail.com>
2022-11-08 08:30:33 +0500

    Quote Teller

---
+++ Qoute Teller/app.js
@@ -0,0 +1,22 @@
+const jokeEl = document.getElementById('joke');
+const get_joke= document.getElementById('get_joke');
+
+get_joke.addEventListener('click' , generateJoke);
+
+generateJoke();
+
+async function generateJoke() {
+     //Call the Icanhaz API
+     const jokeRes = await fetch('https://icanhazdadjoke.com/', {
+          headers: {
+               accept:'application/json'
+          }
+     });
+    
+     const joke = await jokeRes.json();
+
+     console.log(joke);
+ 
+     //Set Random Joke
+     jokeEl.innerHTML = joke.joke;
+}
